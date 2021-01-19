666efb73687c3f026a5bd58352abe1c16f31bfd4
Lyle Rallos <61070788+dubz-skrrrt@users.noreply.github.com>
2021-01-19 16:17:26 +0800

    Initial Commit

---
+++ Summing_Forces/Assets/Scripts/BlinkingText.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkingText : MonoBehaviour
+{ 
+    Text text;
+
+    public GameObject movingCar;
+    public bool startBlinking;
+    void Start()
+    {
+
+        text =GetComponent<Text>();
+        movingCar = GameObject.FindGameObjectWithTag("Car");
+        
+        
+        
+        
+    }
+
+    void Update(){
+        Debug.Log(movingCar.GetComponent<NewtonLaw_movement>().NetForceTrue);
+        
+        FlashingText();
+        // if (startBlinking == true){
+        //     FlashingText();
+        //     startBlinking = false;
+        // }
+    }
+
+
+    void FlashingText(){
+        startBlinking = movingCar.GetComponent<NewtonLaw_movement>().NetForceTrue;
+        if (startBlinking)
+        {
+            StartCoroutine(BlinkingBad());
+            startBlinking = false;
+        }else{
+            StartCoroutine(BlinkingGood());
+        }
+        
+    }
+
+    // Update is called once per frame
+    IEnumerator BlinkingBad()
+    {
+        while(true){
+           
+            text.text= "";
+            
+            yield return new WaitForSeconds(1f);
+            
+            text.text= "ERROR";
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    IEnumerator BlinkingGood()
+    {
+        while(true){
+           
+            // text.text= "";
+            
+            // yield return new WaitForSeconds(1f);
+            
+            text = GetComponent<Text>();
+            text.color = Color.green;
+            text.text= "SUCCESS";
+            
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
