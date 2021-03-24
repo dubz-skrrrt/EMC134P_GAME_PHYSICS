22de1443f18be0c112753b7256a2cd5fcc67ff98
dubz-skrrrt <chill.99.lr@gmail.com>
2021-03-24 14:51:42 +0800

    Goals and conditions of scoring

---
+++ BasketballRange/Assets/Scripts/Scoring.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoring : MonoBehaviour
+{
+    private Text scoreText;
+    private Vector3 offset = new Vector3(0, 40, 20);
+    private Vector3 camOffset = new Vector3(0, -70, 140);
+    public GameObject UI;
+    public GameObject mainCamera;
+    [HideInInspector]
+    public ShootingBall shooting;
+    [HideInInspector]
+    public bool isScored = false;
+    [HideInInspector]
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        scoreText = GameObject.FindGameObjectWithTag("scoreText").GetComponent<Text>();
+        scoreText.text = "Score: " + score;
+        Positioning();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(isScored);
+    }
+
+    void OnTriggerExit(Collider other) {
+        Debug.Log(other.gameObject.name);
+        if(other.gameObject.name == "Baller" && isScored == false){
+            Debug.Log("score!");
+            score++;
+            isScored = true;
+            scoreText.text = "Score: " + score;
+        }
+    }
+
+    public void Positioning()
+    {
+        // shooting = GameObject.FindGameObjectWithTag("GM").GetComponent<ShootingBall>();
+        //Debug.Log(shooting.ball.transform.position.z);
+        UI.transform.position = shooting.curPos + offset;//new Vector3(0, 48, shooting.ball.transform.position.z+28);
+        mainCamera.transform.position = shooting.curPos - camOffset;
+    }
+}
---
+++ BasketballRange/Assets/Scripts/ShootingBall.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShootingBall : MonoBehaviour
+{
+    public GameObject ball;
+    private GameObject baller;
+    private Vector3 throwDirection = new Vector3(0, 140, 70);
+    private Vector3 startPos;
+    [HideInInspector]
+    public Vector3 curPos;
+    private Vector3 forceSpeed;
+
+    private Slider pwrSlider;
+    private bool slideRight = true;
+    
+    public float defaultThrowForce;
+    public float throwForce;
+    public Scoring curScore;
+
+    private bool holdBall = true;
+    private bool shootPress = false;
+    private Transform ballPos;
+    private float resetTimer = 2f;
+
+    void Start()
+    {
+        startPos = ball.transform.position;
+        curPos = startPos;
+        curScore = GameObject.FindGameObjectWithTag("Scorer").GetComponent<Scoring>();
+        pwrSlider = GameObject.FindGameObjectWithTag("powerSlider").GetComponent<Slider>();
+        SpawnBall();
+    }
+
+    void Update()
+    {
+        MovePwrBar();
+        
+        if (holdBall && shootPress)
+        {
+            ballPos.transform.position = baller.transform.position;
+            holdBall = false;
+            baller.GetComponent<Rigidbody>().useGravity = true;   //reapply gravity once the player has released the ball
+            baller.GetComponent<Rigidbody>().AddForce(throwDirection.normalized * throwForce);
+        }
+
+        if (baller != null && ballPos.transform.position.y < 6)
+        {
+            resetTimer -= Time.deltaTime;
+            if(resetTimer <= 0f){
+                ResetBall();
+                resetTimer = 2f;
+            }
+        }
+    }
+
+    void SpawnBall(){
+        DifficultyChange();
+        baller = Instantiate(ball, curPos, ball.transform.rotation) as GameObject;
+        baller.name = "Baller";
+        ballPos = baller.transform;
+        baller.GetComponent<Rigidbody>().useGravity = false;
+        
+    }
+
+    void MovePwrBar(){
+        if(!shootPress){
+            if(pwrSlider.value < 1 && slideRight){
+            pwrSlider.value += 0.002f;
+            }
+            else{
+                slideRight = false;
+                pwrSlider.value -= 0.002f;
+                if(pwrSlider.value <= 0){
+                    slideRight = true;
+                }
+            }
+        }
+    }
+
+    void ResetBall(){
+        Debug.Log("Destroy");
+        Destroy(baller);
+        holdBall = true;
+        shootPress = false;
+        throwDirection = new Vector3(0, 140, 70);
+        pwrSlider.value = 0f;
+        slideRight = true;
+        curScore.isScored = false;
+        SpawnBall();
+    }
+    void DifficultyChange()
+    {
+        if (curScore.score == 1){
+            curPos = new Vector3(ball.transform.position.x, ball.transform.position.y, ball.transform.position.z-80);
+            defaultThrowForce = 10500f;
+        }else if(curScore.score == 2){
+            curPos = new Vector3(ball.transform.position.x, ball.transform.position.y, ball.transform.position.z-140);
+            defaultThrowForce = 11000f;
+
+        }else if(curScore.score == 3){
+            curPos = new Vector3(ball.transform.position.x, ball.transform.position.y, ball.transform.position.z-240);
+            defaultThrowForce = 12000f;
+
+        }else if(curScore.score == 4){
+            curPos = new Vector3(ball.transform.position.x, ball.transform.position.y, ball.transform.position.z-360);
+            defaultThrowForce = 13000f;
+        }else{
+            curPos = new Vector3(ball.transform.position.x, ball.transform.position.y, ball.transform.position.z);
+            defaultThrowForce = 10000f;
+        }
+        curScore.Positioning();
+    }
+    public void FinalForce()
+    {
+        throwForce = defaultThrowForce * pwrSlider.value;
+        shootPress = true;
+    }
+
+    
+}
